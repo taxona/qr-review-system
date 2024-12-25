a348c5501d3226a861e6655612f1be2c0c5d7cf3
taxaontech <145188738+taxona@users.noreply.github.com>
2024-12-25 20:16:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,20 @@
-# config.py
-
 import os
 from dotenv import load_dotenv
 
-# Determine the base directory
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Load environment variables from the .env file (for local development)
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    # Flask Configuration
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
-    # Database Configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # Ensure this line uses the environment variable
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Email Configuration
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
-    # (Optional) reCAPTCHA Configuration
+    # Optional reCAPTCHA
     # RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
     # RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
